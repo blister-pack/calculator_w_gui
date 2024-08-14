b1472a19c642162c2875330172c792e52d634101
blister-pack <andrecrujo@hotmail.com>
2024-08-14 16:11:04 +0200

    calculator layout changes

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function multiplication(a, b) {
 function division(a, b) {
     return a / b;
 }
+
+console.log(addition(3,2));
+console.log(subtraction(3,2));
+console.log(multiplication(3,2));
+console.log(division(3,2));
